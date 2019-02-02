003f5a5343f56d7eaf26083ebe7969af7784e6c2
Sadat Akhtar <shadowak47@yahoo.com>
2019-02-02 13:08:53 +0000

    Create newsFeeder.py
    Feel free to change the website URL's within mysocialsites[]

---
+++ newsFeeder.py
@@ -0,0 +1,24 @@
+import webbrowser
+import time
+
+
+mysocialsites = ['https://google.com', 'https://facebook.com', 'https://twitter.com', 'https://coinmarketcap.com',
+                 'https://youtube.com']
+answer = input('Would you like to add a new webpage to your existing Newsfeeder(y/n)? ')
+
+
+if answer == 'n':
+
+     for x in mysocialsites:
+        webbrowser.open(x)
+        time.sleep(2)
+
+else:
+    newWebsite = input('Please enter new website url: ')
+    mysocialsites.append(newWebsite)
+    #print(mysocialsites)
+
+    for y in mysocialsites:
+        webbrowser.open(y)
+        time.sleep(2)
+
