1abf9d2540c8a137ea2b4c1fe18f0a2cd0632956
Sadat <sadatakhtar@hotmail.co.uk>
2019-02-02 21:32:43 +0000

    Made some alterations to the program, added in a nested if statement

--- newsFeeder.py
+++ newsFeeder.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import webbrowser
 import time
 
@@ -6,19 +7,24 @@ mysocialsites = ['https://google.com', 'https://facebook.com', 'https://twitter.
                  'https://youtube.com']
 answer = input('Would you like to add a new webpage to your existing Newsfeeder(y/n)? ')
 
+if answer == 'y' or answer == 'n':
 
-if answer == 'n':
+    if answer == 'n':
 
-     for x in mysocialsites:
-        webbrowser.open(x)
-        time.sleep(2)
+        for x in mysocialsites:
+            webbrowser.open(x)
+            time.sleep(2)
 
+    else:
+        newWebsite = input('Please enter new website url: ')
+        mysocialsites.append(newWebsite)
+        #print(mysocialsites)
+
+        for y in mysocialsites:
+            webbrowser.open(y)
+            time.sleep(2)
 else:
-    newWebsite = input('Please enter new website url: ')
-    mysocialsites.append(newWebsite)
-    #print(mysocialsites)
-
-    for y in mysocialsites:
-        webbrowser.open(y)
-        time.sleep(2)
+    for x in mysocialsites:
+        webbrowser.open(x)
+        time.sleep(2)   
 
